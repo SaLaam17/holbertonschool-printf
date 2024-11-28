0b557da270b71aebc604f31c16a2bfa1dc0df530
SaLaam17 <slaamri@yahoo.fr>
2024-11-28 09:24:38 +0100

    'move -putchar from main.h to functions_t_print.c'

--- functions_t_print.c
+++ functions_t_print.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * print_char - function that print a char
  * @args: A va_list pointing to the character to be printed.
  * Return: the number of characters printed
--- main.h
+++ main.h
@@ -13,19 +13,6 @@ int print_int(va_list args);
 int print_percent(va_list args);
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * struct type_format - new struct type
  * @letter: a char representing a data type.
  * @function_pointer: function pointer to a function that prints
